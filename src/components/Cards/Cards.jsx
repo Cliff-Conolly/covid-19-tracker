e18ac459c37cb41890b622c53990ca9e5b8abe9c
Cliff Conolly <cliffconolly@protonmail.com>
2020-06-29 14:47:03 +0100

    imported Card, CardContent, Typography, Grid from @material-ui/core

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 // Props brought in from Cards component in App.js:
 const Cards = (props) => {
