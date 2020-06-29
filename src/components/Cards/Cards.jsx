9380f69c2bdfd481645c85c648c2e2db14aaf9dc
Cliff Conolly <cliffconolly@protonmail.com>
2020-06-29 15:14:10 +0100

    built out the styles for the Cards.jsx component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
+
+import styles from './Cards.module.css';
 
 // Props brought in from Cards component in App.js:
 const Cards = (props) => {
-  console.log(props);
+  // console.log(props);
+
+
  return (
-  <h1>Cards</h1>
- )
+  <div className={styles.container}>
+      <Grid container spacing ={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+  </div>
+ );
 }
 
 export default Cards;
