088ee4637da033afe1775d27327798739ee4dd40
Cliff Conolly <cliffconolly@protonmail.com>
2020-06-29 16:06:59 +0100

    destructured the const Cards data to add the prop 'data' from App.js

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,10 +3,12 @@ import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-u
 
 import styles from './Cards.module.css';
 
-// Props brought in from Cards component in App.js:
-const Cards = (props) => {
-  // console.log(props);
-
+// Props brought in from Cards component in App.js; props then destructured to the specific values
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+  console.log(confirmed);
+  if(!confirmed) {
+    return 'Loading...';
+  }
 
  return (
   <div className={styles.container}>
@@ -14,9 +16,25 @@ const Cards = (props) => {
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of deaths from COVID-19</Typography>
           </CardContent>
         </Grid>
       </Grid>
